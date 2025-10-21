49c516f75a0cc9af18b821d378e56a502741c100
Luis Compean <fernando.compean0613@gmail.com>
2025-10-20 21:16:07 -0500

    Update MainActivity with bottom navigation scaffold

--- app/src/main/java/com/example/gihealth/MainActivity.kt
+++ app/src/main/java/com/example/gihealth/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.gihealth.ui.theme.GIHealthTheme
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.gihealth.ui.screens.AddNewScreen
+import com.example.gihealth.ui.screens.AnalyticsScreen
+import com.example.gihealth.ui.screens.FoodScreen
+import com.example.gihealth.ui.screens.JournalScreen
+import com.example.gihealth.ui.screens.SymptomScreen
+import com.example.gihealth.utils.Constants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +30,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GIHealthTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                Surface(color = Color.White) {
+                    //Scaffold Component
+                    Scaffold(
+                        //Bottom navigation bar
+                        bottomBar = {
+                            BottomNavigationBar(navController = navController)
+                        },
+                        content = { padding ->
+                            //Main Navigation Container
+                            NavHostContainer(
+                                navController = navController,
+                                padding = padding
+                            )
+                        }
                     )
                 }
             }
@@ -31,17 +53,89 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavHostContainer(
+    navController: NavHostController,
+    padding: PaddingValues
+) {
+    NavHost(
+        navController = navController,
+
+        // set the start destination as food screen
+        startDestination = "food",
+
+        // Set the padding provided by scaffold
+        modifier = Modifier.padding(paddingValues = padding),
+
+        builder = {
+            // route : go to food screen
+            composable("food") {
+                FoodScreen()
+            }
+            // route : go to symptom screen
+            composable("symptoms") {
+                SymptomScreen()
+            }
+            // route : go to Add New screen
+            composable("add") {
+                AddNewScreen()
+            }
+            // route : go to Journal screen
+            composable("journal") {
+                JournalScreen()
+            }
+            // route : go to Analytics screen
+            composable("analytics") {
+                AnalyticsScreen()
+            }
+        })
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GIHealthTheme {
-        Greeting("Android")
+fun BottomNavigationBar(navController: NavHostController) {
+    NavigationBar(
+        // set background color of the bottom bar
+        containerColor = Color(0xFF0F9D58)) {
+
+        // observe the backstack
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+        // observe current route to change the icon
+        // color,label color when navigated
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        // Bottom nav items we declared
+        Constants.BottomNavItems.forEach { navItem ->
+
+            // Place the bottom nav items
+            NavigationBarItem(
+
+                // it currentRoute is equal then its selected route
+                selected = currentRoute == navItem.route,
+
+                // navigate on click
+                onClick = {
+                    navController.navigate(navItem.route)
+                },
+
+                // Icon of navItem
+                icon = {
+                    Icon(imageVector = navItem.icon, contentDescription = navItem.label)
+                },
+
+                // label
+                label = {
+                    Text(text = navItem.label)
+                },
+                alwaysShowLabel = true,
+
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.White, // Icon color when selected
+                    unselectedIconColor = Color.White, // Icon color when not selected
+                    selectedTextColor = Color.White, // Label color when selected
+                    indicatorColor = Color(0xFF195334) // Highlight color for selected item
+                )
+            )
+        }
     }
 }
+
